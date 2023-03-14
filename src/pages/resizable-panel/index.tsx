90a7291208c30e159caea01a2322fc51355c2e37
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-03-13 23:53:43 -0400

    Add react-use-measure package and update height calculation in ResizablePanel component for better resizing experience

--- src/pages/resizable-panel/index.tsx
+++ src/pages/resizable-panel/index.tsx
@@ -1,11 +1,13 @@
 import { AnimatePresence, motion, MotionConfig } from "framer-motion";
 import { useState, useContext, createContext } from "react";
 import { CheckIcon } from "@heroicons/react/20/solid";
+import useMeasure from "react-use-measure";
 
-const transition = { type: "ease", ease: "easeInOut", duration: 1 };
+const transition = { type: "ease", ease: "easeInOut", duration: 0.4 };
 
 export default function ResizablePanel() {
   const [status, setStatus] = useState("idle");
+  const [ref, bounds] = useMeasure();
 
   return (
     <MotionConfig transition={transition}>
@@ -17,47 +19,61 @@ export default function ResizablePanel() {
             </div>
             <motion.div
               animate={{
-                height: status === "idle" || status === "saving" ? 205 : 84,
+                height: bounds.height > 0 ? bounds.height : "null",
               }}
+              transition={{ type: "spring", bounce: 0.2, duration: 0.8 }}
             >
-              <AnimatePresence mode="wait">
-                {status === "idle" || status === "saving" ? (
-                  <motion.div
-                    exit={{ opacity: 0 }}
-                    transition={{...transition, duration: transition.duration / 2}}
-                    key="form"
-                  >
-                    <Form
-                      onSubmit={async () => await delay(1000)}
-                      afterSave={() => setStatus("success")}
-                      className="p-8"
+              <div ref={ref}>
+                <AnimatePresence mode="popLayout">
+                  {status === "idle" || status === "saving" ? (
+                    <motion.div
+                      exit={{ opacity: 0 }}
+                      transition={{
+                        ...transition,
+                        duration: transition.duration / 2,
+                      }}
+                      key="form"
                     >
-                      <p className="text-sm text-zinc-400">
-                        Enter your email to get a password reset link:
+                      <Form
+                        onSubmit={async () => await delay(1000)}
+                        afterSave={() => setStatus("success")}
+                        className="p-8"
+                      >
+                        <p className="text-sm text-zinc-400">
+                          Enter your email to get a password reset link:
+                        </p>
+                        <div className="mt-3">
+                          <input
+                            className="block w-full rounded border-none text-slate-900"
+                            type="email"
+                            required
+                            defaultValue="sam@buildui.com"
+                          />
+                        </div>
+                        <div className="mt-8 text-right">
+                          <Form.Button className="rounded bg-indigo-500 px-5 py-2 text-sm font-medium text-white ">
+                            Email me my link
+                          </Form.Button>
+                        </div>
+                      </Form>
+                    </motion.div>
+                  ) : (
+                    <motion.div
+                      transition={{
+                        ...transition,
+                        duration: transition.duration / 2,
+                        delay: transition.duration / 2,
+                      }}
+                      initial={{ opacity: 0 }}
+                      animate={{ opacity: 1 }}
+                    >
+                      <p className="p-8 text-sm text-zinc-400">
+                        Email sent! Check your inbox to continue.
                       </p>
-                      <div className="mt-3">
-                        <input
-                          className="block w-full rounded border-none text-slate-900"
-                          type="email"
-                          required
-                          defaultValue="sam@buildui.com"
-                        />
-                      </div>
-                      <div className="mt-8 text-right">
-                        <Form.Button className="rounded bg-indigo-500 px-5 py-2 text-sm font-medium text-white ">
-                          Email me my link
-                        </Form.Button>
-                      </div>
-                    </Form>
-                  </motion.div>
-                ) : (
-                  <motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-                    <p className="p-8 text-sm text-zinc-400">
-                      Email sent! Check your inbox to continue.
-                    </p>
-                  </motion.div>
-                )}
-              </AnimatePresence>
+                    </motion.div>
+                  )}
+                </AnimatePresence>
+              </div>
             </motion.div>
           </div>
 
