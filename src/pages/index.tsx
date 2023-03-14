10894fabdafc0af09b717c87de9c256270bf1f54
Ismael Herrera <ismaelherrera.dr@outlook.com>
2023-03-13 21:18:45 -0400

    Add padding to main container for better user experience.git commit -m "Add padding to main container for better user experience"

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -13,7 +13,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={inter.className}>
-        <div className="flex min-h-screen items-start bg-gradient-to-r from-blue-500 to-blue-600 pt-40">
+        <div className="flex min-h-screen items-start bg-gradient-to-r from-blue-500 to-blue-600 pt-40 px-6">
           <div className="mx-auto w-full max-w-md overflow-hidden rounded-2xl bg-white">
             <div className="grid grid-cols-1 divide-y divide-gray-400">
               <Link
